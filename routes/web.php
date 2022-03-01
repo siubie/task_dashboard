ff15467a467011f09b160adb7981ce364d79bfd6
mrefawaladam <aerfa55@gmail.com>
2022-03-02 06:26:17 +0700

    setup root

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RootController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[RootController::class,'index']);
+
 
 Auth::routes();
 
