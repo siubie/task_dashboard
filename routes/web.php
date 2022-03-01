c9108b1ba51fba0dbe0164239dc9f1a33845a56b
AndreMaulanaMustofa <andremm73@gmail.com>
2022-03-02 01:09:31 +0700

    Pretest 3

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/baru', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
