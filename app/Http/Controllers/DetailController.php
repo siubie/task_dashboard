1c83e7bd54e0a201bf18faa01dbd671d39542300
mrefawaladam <aerfa55@gmail.com>
2022-03-02 10:40:23 +0700

    fix layout

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -10,13 +10,14 @@ class DetailController extends Controller
                 
         $data = [
             'newsTitle' => 'Ini Judul Hero Post dari controller',
-            'newsDescription' => 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.',
+            'newsDecsription' => 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.',
             'featuredPost1' => 'Ini judul featured post 1',
             'featuredPost2' => 'Ini judul featured post 2',
             'content1' => 'ini content featured post 1 dari controller',
             'content2' => 'ini content featured post 2 dari controller'
         ];
 
-        return view('news.hero-post-detail',$data);
+        return view('welcome',$data);
     }
+    
 }
--- app/Http/Controllers/FeaturedController.php
+++ app/Http/Controllers/FeaturedController.php
@@ -10,14 +10,14 @@ class FeaturedController extends Controller
 
         $data = [
             'newsTitle' => 'Ini judul featured post '.$id,
-            'newsDescription' => 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.',
+            'newsDecsription' => 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.',
             'featuredPost1' => 'Ini judul featured post 1',
             'featuredPost2' => 'Ini judul featured post 2',
             'content1' => 'ini content featured post 1 dari controller',
             'content2' => 'ini content featured post 2 dari controller'
         ];
 
-        return view('news.featured',$data);
+        return view('welcome',$data);
 
       
     }
