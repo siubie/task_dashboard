7754b8b88b8d2131ccc6539101e0696eb2c74206
= <=>
2022-03-02 12:18:28 +0700

    pretes 3

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\DetailController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,13 +15,11 @@ use Illuminate\Support\Facades\Auth;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', [DetailController::class, 'index']);
 
-Route::get('/news/featured/{id}', [App\HttpController\DetailController::class, 'featured']);
+Route::get('/news/featured/{id}', [App\Http\Controllers\DetailController::class, 'featured']);
 
-Route::get('/news/hero-post-detail', [App\Http\Controller\DetailController::class, 'show'])->name('detail');
-
+Route::get('/news/hero-post-detail', [App\Http\Controllers\DetailController::class, 'show'])->name('detail');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
