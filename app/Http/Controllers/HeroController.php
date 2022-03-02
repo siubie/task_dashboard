ff37f14ed9061e21948e6775ca018d4ed7086309
Annisa Mutiara <14tiaraannisa@gmail.com>
2022-03-02 11:18:14 +0700

    ubah tampilan welcome -> blog

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -8,6 +8,9 @@ class HeroController extends Controller
 {
     public function index()
     {
-        # code...
+        $title = 'Ini Judul Hero Post dari controller';
+        $desc = 'Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.';
+        $url = url('/');
+        return view('index', compact('title', 'desc', 'url'));
     }
 }
