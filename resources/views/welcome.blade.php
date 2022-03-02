8ebc1b57f97a78d8119d688dd41359c95be23ed2
= <=khofifahmanda04@gmail.com>
2022-03-03 00:55:04 +0700

    comit coba route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,11 +78,12 @@
   </div>
 </div>
 
+{{-- ini --}}
 <main class="container">
   <div class="p-4 p-md-5 mb-4 text-white rounded bg-dark">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 fst-italic">{{$tittle}}</h1>
-      <p class="lead my-3">{{$desc}}</p>
+      <h1 class="display-4 fst-italic">"{{$tittle}}"</h1>
+      <p class="lead my-3">"{{$desc}}"</p>
       <p class="lead mb-0"><a href="{{$url}}" class="text-white fw-bold">Continue reading...</a></p>
     </div>
   </div>
